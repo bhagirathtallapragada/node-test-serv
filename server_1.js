c26093256273f1daea6569ab32fccadb0380c9c5
Bhagirath <bhagirathtallapragada@gmail.com>
2019-04-10 14:14:05 +0530

    practice entry

---
+++ server_1.js
@@ -0,0 +1,7 @@
+const http=require('http')
+let server=http.createServer((req,res)=>{
+    res.write('hi from http server')
+    res.end()
+})
+server.listen(3000)
+console.log('server is listenning at 3000')
